70fd5dc3666ec91541a6c70adb7a1f2a35d31ccb
kkangdaeun <164451449+kkangdaeun@users.noreply.github.com>
2025-11-05 00:27:51 +0900

    Update locations.js

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,206 +1,44 @@
-const { render } = require('pug');
-var request = require('request');
+const mongoose = require('mongoose');
+const readLine = require('readline');
+mongoose.set("strictQuery", false);
 
-const apiOptions = {
-  server: "http://localhost:3000"
-};
-if (process.env.NODE_ENV === 'production') {
-  apiOptions.server = "https://loc8r-api-pp09.onrender.com";
-}
-const requestOptions ={
-  url: `${apiOptions.server}`,
-  method: "GET",
-  json: {},
-  qs: {
-    offset:20,
-  }
-};
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbURI = `mongodb+srv://myatlasdbuser:${dbPassword}@cluster0.kqfrowx.mongodb.net/Loc8r`;
+mongoose.connect(dbURI, { useNewUrlParser: true});
 
-request(requestOptions, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } if (response.statusCode === 200) {
-    console.log(body);
-  } else {
-    console.log(response.statusCode);
-  }
+mongoose.connection.on('connected', function(){
+ console.log(`Mongoose connected to ` + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+ console.log(`Mongoose connection error: ` + err);
+});
+mongoose.connection.on('disconnected', function() {
+ console.log('Mongoose disconnected');
 });
 
-const homelist = (req, res) => {
-  const path = '/api/locations';
-  const requestOptions = {
-    url: `${apiOptions.server}${path}`,
-    method: 'GET',
-    json: {},
-    qs: {
-      lng: 127.262074,
-      lat: 37.068769,
-      maxDistance: 900000
-    }
-  };
-  request(
-    requestOptions,
-    (err, {statusCode}, body) => {
-      let data = [];
-      if (statusCode === 200 && body.length) {
-      data = body.map((item) => {
-        item.distance = formatDistance(item.distance);
-        return item;
-      });
-    };
-      renderHomepage(req, res, data);
-    }
-  );
+var gracefulShutdown = function (msg, callback) {
+ mongoose.connection.close( function(){
+ console.log(`Mongoose disconnected through` + msg);
+ callback();
+ });
 };
+// For nodemon restarts
+process.once('SIGUSR2', function(){
+ gracefulShutdown('nodemon restart', function() {
+ process.kill(process.pid, 'SIGUSR2');
+ });
+});
+// For app termination
+process.on('SIGINT', function() {
+ gracefulShutdown('app termination', function() {
+ process.exit(0);
+ });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+ gracefulShutdown('Heroku app shutdown', function() {
+ process.exit(0);
+ });
+});
 
-const formatDistance = (distance) => {
-  let thisDistance = 0;
-  let unit = 'm';
-  if (distance > 1000) {
-    thisDistance = parseFloat(distance / 1000).toFixed(1);
-    unit = 'km';
-  } else {
-    thisDistance = Math.floor(distance);
-  } 
-  return thisDistance + unit;
-};
-
-const renderDetailPage = (req, res, location) => {
-  res.render('location-info', {
-    title: location.name,
-    pageHeader: {
-      title: location.name
-    },
-    sidebar: {
-      context: 'is on Loc8r because it has accessible wifi and \
-space to sit down with your laptop and get some work done.(2023720160 강다은)',
-      callToAction: "If you've been and you like it - or if you \
-don't - please leave a review to help other people just like you."
-    },
-    location
-  });
-};
-
-const renderHomepage = (req, res, responseBody) => {
-  let message = null;
-  if (!(responseBody instanceof Array)) {
-    message = "API lookup error";
-    responseBody = [];
-  } else {
-    if (!responseBody.length) {
-      message = "No places found nearby";
-    }
-  }
-  res.render('location-list', {
-    title: 'Loc8r - find a place to work with wifi',
-    pageHeader: {
-      title: 'Loc8r',
-      strapline: 'Find places to work with wifi near you!'
-    },
-    sidebar: "Looking for wifi and a seat? Loc8r helps you find places \
-to work when out and about. Perhaps with coffee, cake or a pint? \
-Let Loc8r help you find the place you're looking for.(2023720160 강다은)",
-    locations: responseBody,
-    message
-  });
-};
-
-const showError = (req, res, status) => {
-  let title = '';
-  let content = '';
-  if (status === 404) {
-    title = '404, page not found';
-    content = 'Oh dear. Looks like you can\'t find this page. Sorry.';
-  } else {
-    title = `${status}, something's gone wrong`;
-    content = 'Something, somewhere, has gone just a little bit wrong.';
-  }
-  res.status(status);
-  res.render('generic-text', {
-    title,
-    content
-  });
-};
-
-const renderReviewForm = function (req, res, {name}) {
-  res.render('location-review-form', {
-    title: `Review ${name} on Loc8r`,
-    pageHeader: { title: `Review ${name}` },
-    error: req.query.err
-  });
-};
-
-const getLocationInfo = (req, res, callback) => {
-  const path = `/api/locations/${req.params.locationid}`;
-  const requestOptions = {
-    url: `${apiOptions.server}${path}`,
-    method: 'GET',
-    json: {}
-  };
-  request(
-    requestOptions,
-    (err, { statusCode }, body) => {
-      let data = body;
-      if (statusCode === 200) {
-        data.coords = {
-          lng: body.coords[0],
-          lat: body.coords[1]
-        };
-        callback(req, res, data);
-      } else {
-        showError(req, res, statusCode);
-      }
-    }
-  );
-};
-
-const locationInfo = (req, res) => {
-  getLocationInfo(req, res,
-    (req, res, responseData) => renderDetailPage(req, res, responseData)
-  );
-};
-
-const addReview = (req, res) => {
-  getLocationInfo(req, res,
-    (req, res, responseData) => renderReviewForm(req, res, responseData)
-  );
-};
-
-const doAddReview = (req, res) => {
-  const locationid = req.params.locationid;
-  const path = `/api/locations/${locationid}/reviews`;
-  const postdata = {
-    author: req.body.name,
-    rating: parseInt(req.body.rating, 10),
-    reviewText: req.body.review
-  };
-  const requestOptions = {
-    url: `${apiOptions.server}${path}`,
-    method: 'POST',
-    json: postdata
-  };
-  if (!postdata.author || !postdata.rating || !postdata.reviewText) {
-     res.redirect(`/location/${locationid}/review/new?err=val`);
-   } else {
-    request(
-      requestOptions,
-      (err, {statusCode}, {name}) => {
-        if (statusCode === 201) {
-          res.redirect(`/location/${locationid}`);
-        } else if (statusCode === 400 && name && name === 'ValidationError') {
-          res.redirect(`/location/${locationid}/review/new?err=val`);
-        } else {
-          showError(req, res, statusCode);
-        }
-      }
-    );
-  }
-};
-
-
-module.exports = {
-  homelist,
-  locationInfo,
-  addReview,
-  doAddReview
-};
+require('./locations');
